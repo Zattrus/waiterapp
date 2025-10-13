bb79344e97d1e1d8f1c57a6dc6d5ee7b260092c4
Gabriel <oliveiragabriel1712@gmail.com>
2025-10-13 18:38:43 -0300

    feat: Implement category selection and adjust component styling

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 import { categories } from "../../mocks/categories";
 import { Text } from "../Text";
 import { Category, Icon } from "./styles";
 
 export function Categories() {
-  return (
+  const [selectedCategory, setSelectedCategory] = useState('');
 
+  function handleCategoryPress(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+
+    setSelectedCategory(category);
+  }
+
+  return (
     <FlatList
       horizontal
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={category => category._id}
       showsHorizontalScrollIndicator={false}
-      renderItem={({ item: category }) => (
-        <Category key={category._id}>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
 
-          <Text size={14} weight="600">{category.name}</Text>
-        </Category>
-      )}
+        return (
+          <Category onPress={() => handleCategoryPress(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {category.icon}
+              </Text>
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        )
+      }}
     />
   )
 }
