674724370ed5f1fd608ca430a78e0ddd4808f104
Gabriel <oliveiragabriel1712@gmail.com>
2025-10-23 18:46:14 -0300

    feat: Adiciona funcionalidade de cancelamento de pedido e exibição da mesa selecionada no cabeçalho

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -19,10 +19,17 @@ export function Main() {
   function handleSaveTable(table: string) {
     setSelectedTable(table);
   }
+
+  function handleCancelOrder() {
+    setSelectedTable('');
+  }
   return (
     <>
       <Container>
-        <Header />
+        <Header
+          selectedTable={selectedTable}
+          onCancelOrder={handleCancelOrder}
+        />
 
         <CategoriesContainer>
           <Categories />
