04c932dce4978ac7548a7f2740b7786388072052
Gabriel <oliveiragabriel1712@gmail.com>
2025-10-23 20:04:28 -0300

    feat: Adiciona modal de produto e refatora componente de menu

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -2,45 +2,58 @@ import { FlatList } from "react-native";
 import { products } from "../../mocks/products";
 import { Text } from "../Text";
 
+import { useState } from "react";
+import { Product } from "../../types/product";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { ProductModal } from "../ProductModal";
 import {
   AddToCartButton,
-  Product,
+  ProductContainer,
   ProductDetails,
   ProductImage,
   Separator
 } from "./styles";
 export function Menu() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
   return (
+    <>
+      <ProductModal visible={isModalVisible} onClose={() => setIsModalVisible(false)} product={selectedProduct} />
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={product => product._id}
+        ItemSeparatorComponent={() => <Separator />}
+        renderItem={({ item: product }) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.2.132:3001/uploads/${product.imagePath}`,
+              }}
+            />
 
-    <FlatList
-      data={products}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      keyExtractor={product => product._id}
-      ItemSeparatorComponent={() => <Separator />}
-      renderItem={({ item: product }) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://192.168.2.132:3001/uploads/${product.imagePath}`,
-            }}
-          />
+            <ProductDetails>
+              <Text weight={"600"}>{product.name}</Text>
+              <Text size={14} color={"#666"} style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text weight={"600"} size={14}>{formatCurrency(product.price)}</Text>
+            </ProductDetails>
 
-          <ProductDetails>
-            <Text weight={"600"}>{product.name}</Text>
-            <Text size={14} color={"#666"} style={{ marginVertical: 8 }}>
-              {product.description}
-            </Text>
-            <Text weight={"600"} size={14}>{formatCurrency(product.price)}</Text>
-          </ProductDetails>
-
-          <AddToCartButton >
-            <PlusCircle />
-          </AddToCartButton>
-        </Product>
-      )}
-    />
+            <AddToCartButton >
+              <PlusCircle />
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+      />
+    </>
   )
 }
---
+++ app/src/components/ProductModal/styles.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components/native";
+
+export const Image = styled.ImageBackground`
+  width: 100%;
+  height: 200px;
+  align-items: flex-end;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+  width: 32px;
+  height: 32px;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 16px;
+  justify-content: center;
+  align-items: center;
+  margin: 24px;
+`;
+
+export const Header = styled.View``;
+export const ModalBody = styled.View`
+  flex: 1;
+  background-color: #fafafa;
+  padding: 32px 24px 0px;
+`;
+
+export const IngredientsContainer = styled.View`
+  margin-top: 32px;
+  flex: 1;
+`;
+
+export const Ingredient = styled.View`
+  border: 1px rgba(204, 204, 204, 0.3);
+  border-radius: 8px;
+  padding: 16px;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 4px;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #fff;
+  padding: 16px 24px;
+`;
+export const FooterContainer = styled.SafeAreaView`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const PriceContainer = styled.View``;
